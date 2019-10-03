403a28f5497b38bf0ddb5cd4b91b068fbeb396c7
mrmlnc <mrmlnc@yandex-team.ru>
2019-10-03 12:13:07 +0300

    style(fs.walk): fix `consistent-type-definitions` rule

--- packages/fs/fs.walk/src/settings.ts
+++ packages/fs/fs.walk/src/settings.ts
@@ -9,7 +9,7 @@ export type DeepFilterFunction = FilterFunction<Entry>;
 export type EntryFilterFunction = FilterFunction<Entry>;
 export type ErrorFilterFunction = FilterFunction<Errno>;
 
-export interface Options {
+export type Options = {
 	basePath?: string;
 	concurrency?: number;
 	deepFilter?: DeepFilterFunction;
@@ -20,7 +20,7 @@ export interface Options {
 	pathSegmentSeparator?: string;
 	stats?: boolean;
 	throwErrorOnBrokenSymbolicLink?: boolean;
-}
+};
 
 export default class Settings {
 	public readonly basePath?: string = this._getValue(this._options.basePath, undefined);
--- packages/fs/fs.walk/src/types/index.ts
+++ packages/fs/fs.walk/src/types/index.ts
@@ -3,7 +3,7 @@ import * as scandir from '@nodelib/fs.scandir';
 export type Entry = scandir.Entry;
 export type Errno = NodeJS.ErrnoException;
 
-export interface QueueItem {
+export type QueueItem = {
 	dir: string;
 	base?: string;
-}
+};

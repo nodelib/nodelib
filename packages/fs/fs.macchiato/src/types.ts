077651a46f2c2f33c8a539e2ef1cf967ecc889b4
mrmlnc <mrmlnc@yandex-team.ru>
2019-10-03 19:48:52 +0300

    style(fs.macchiato): fix `indent` rule

--- packages/fs/fs.macchiato/src/types.ts
+++ packages/fs/fs.macchiato/src/types.ts
@@ -7,11 +7,11 @@ export type UnboxReturnTypeFromClassItem<T> =
 	 * Unbox return type from class method.
 	 */
 	T extends (...args: unknown[]) => infer TReturnType ? TReturnType :
-	/**
-	 * Unbox return type from class property.
-	 */
-	T extends (infer TReturnType) ? TReturnType :
-	/**
-	 * Default branch. Potentially impossible case.
-	 */
-	T;
+		/**
+		 * Unbox return type from class property.
+		 */
+		T extends (infer TReturnType) ? TReturnType :
+			/**
+			 * Default branch. Potentially impossible case.
+			 */
+			T;

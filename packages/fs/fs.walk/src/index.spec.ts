870382d7e37feeee6d17f0e7740debc3f21f5b5a
mrmlnc <mrmlnc@yandex-team.ru>
2019-10-03 12:13:49 +0300

    style(fs.walk): fix `no-require-imports` rule

--- packages/fs/fs.walk/src/index.spec.ts
+++ packages/fs/fs.walk/src/index.spec.ts
@@ -2,7 +2,7 @@ import * as assert from 'assert';
 import * as fs from 'fs';
 import { Readable } from 'stream';
 
-import rimraf = require('rimraf');
+import * as rimraf from 'rimraf';
 
 import * as pkg from './index';
 import { Errno } from './types';
